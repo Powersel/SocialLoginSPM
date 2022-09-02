17d46ca39d537aa59108dea62f6957ff1dd6add6
Sergiy.Shevchuk <Sergiy.Shevchuk@icloud.com>
2022-09-02 10:59:32 +0300

    Added socialLogin button

--- Sources/SocialLogin/Presentation/SharableElements/SocialLoginButton.swift
+++ Sources/SocialLogin/Presentation/SharableElements/SocialLoginButton.swift
@@ -7,25 +7,131 @@
 
 import UIKit
 
-final class SocialLoginButton: UIView {
-    private let titleLabel = UILabel()
-    private var thumbnailImage = UIImageView()
+enum SocialButtonViewType {
+    case google
+    case facebook
+    case twitter
+    case linkedin
+    case apple
     
-    private let type: SocialButtonType
-    
-    init(type: SocialButtonType) {
-        self.type = type
-        
-        super.init(frame: CGRect.zero)
-        
-        self.configureUI()
+    var backgroundColor: UIColor {
+        switch self {
+        case .google: return UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+        case .facebook: return UIColor(red: 66/255, green: 103/255, blue: 178/255, alpha: 1)
+        case .twitter: return UIColor(red: 29/255, green: 161/255, blue: 242/255, alpha: 1)
+        case .linkedin: return UIColor(red: 40/255, green: 103/255, blue: 178/255, alpha: 1)
+        case .apple: return .black
+        }
     }
+    
+    var textColor: UIColor {
+        switch self {
+        case .google: return UIColor(red: 3/255, green: 3/255, blue: 3/255, alpha: 0.54)
+        case .facebook,
+             .twitter,
+             .linkedin,
+             .apple:
+            return .white
+        }
+    }
+    
+    var icon: UIImage? {
+        switch self {
+        case .google: return nil
+        case .facebook: return nil
+        case .twitter: return nil
+        case .linkedin: return nil
+        case .apple: return nil
+        }
+    }
+    
+    var loginText: String {
+        switch self {
+        case .google: return "Continue with Google"
+        case .facebook: return "Continue with Facebook"
+        case .twitter: return "Continue with Twitter"
+        case .linkedin: return "Continue with Linkedin"
+        case .apple: return "Continue with Apple"
+        }
+    }
+    
+    var registerText: String {
+        switch self {
+        case .google: return "Register with Google"
+        case .facebook: return "Register with Facebook"
+        case .twitter: return "Register with Twitter"
+        case .linkedin: return "Register with Linkedin"
+        case .apple: return "Register with Apple"
+        }
+    }
+    
+    var outlineColor: UIColor {
+        switch self {
+        case .google: return UIColor.red
+        default: return UIColor.green
+        }
+    }
+}
+
+@IBDesignable
+class SocialButtonView: UIView {
+    private var contentView: UIView?
+    
+    @IBOutlet private var imageView: UIImageView!
+    @IBOutlet var button: UIButton!
     
     required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: aDecoder)
+        commonInit()
     }
     
-    func configureUI() {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    func configureForLogin(with type: SocialButtonViewType) {
+        backgroundColor = type.backgroundColor
+        button.setTitleColor(type.textColor, for: .normal)
+        button.setTitle(type.loginText, for: .normal)
         
+        layer.borderWidth = 1
+        layer.borderColor = type.outlineColor.cgColor
+        layer.cornerRadius = frame.height / 2
+        
+        imageView.image = type.icon
+    }
+    
+    func configureForRegistration(with type: SocialButtonViewType) {
+        backgroundColor = type.backgroundColor
+        button.setTitleColor(type.textColor, for: .normal)
+        button.setTitle(type.registerText, for: .normal)
+
+        layer.borderWidth = 1
+        layer.borderColor = type.outlineColor.cgColor
+        layer.cornerRadius = frame.height / 2
+        
+        imageView.image = type.icon
+    }
+}
+
+private extension SocialButtonView {
+    func configure() {
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 14.0, weight: .semibold)
+    }
+    
+    func commonInit() {
+        guard let view = loadViewFromNib() else { return }
+        view.frame = self.bounds
+        self.addSubview(view)
+        contentView = view
+        
+        configure()
+    }
+    
+    func loadViewFromNib() -> UIView? {
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: "SocialButtonView", bundle: bundle)
+        return nib.instantiate(withOwner: self, options: nil).first as? UIView
     }
 }
