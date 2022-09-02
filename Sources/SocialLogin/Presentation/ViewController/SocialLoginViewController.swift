3a7616951819d55ab5103a8b291a67eb9b68c6d8
Sergiy.Shevchuk <Sergiy.Shevchuk@icloud.com>
2022-09-02 10:50:03 +0300

    Added controller files

---
+++ Sources/SocialLogin/Presentation/ViewController/SocialLoginViewController.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  
+//
+//  Created by Powersel on 02.09.2022.
+//
+
+import UIKit
+import Foundation
+
+public final class SocialLoginViewController: UIViewController {
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
+private extension SocialLoginViewController {
+    func configureUI() {
+        
+    }
+}
